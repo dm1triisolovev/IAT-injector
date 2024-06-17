6c9c45b999b9d9c2e4be73a2ac40df2840d12d31
Dmitrii <thesomeguynextdoor@gmail.com>
2024-06-17 21:19:37 +0300

    add: main precompile header to usermode

---
+++ usermode/usermode/stdafx/stdafx.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Windows.h>
+
+#include <iostream>
+#include <cstdint>
+#include <array>
+#include <list>
+#include <functional>
+#include <fstream>
+#include <locale>
+#include <codecvt>
+#include <vector>
