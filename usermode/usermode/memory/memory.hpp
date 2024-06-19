649bba7b9f25502e900a8f930bfaed604ecbd0a8
Dmitrii <thesomeguynextdoor@gmail.com>
2024-06-19 22:06:25 +0300

    add: function [read]

--- usermode/usermode/memory/memory.hpp
+++ usermode/usermode/memory/memory.hpp
@@ -9,10 +9,20 @@
 
 struct memory_t {
 	NTSTATUS read_buffer( uint64_t, LPVOID, SIZE_T );
-	NTSTATUS write_memory( uint64_t address, uintptr_t dstAddress, SIZE_T nSize );
+	NTSTATUS write_memory( uint64_t, uintptr_t, SIZE_T );
+
+	template<typename T>
+	T read( uint64_t address );
 
 	uint64_t process_id = 0;
 	operation_callback operation_callback;
 };
+
+template<typename T>
+inline T memory_t::read( uint64_t address ) {
+	T buffer{};
+	read_buffer( address, &buffer, sizeof( T ) );
+	return buffer;
+}
 
 extern std::unique_ptr<memory_t> memory;
