74a80f95beaa6d89acb26383efe6661331420975
Dmitrii <thesomeguynextdoor@gmail.com>
2024-06-19 21:38:22 +0300

    add: fn alloc memory

--- usermode/usermode/communiction/communication.cpp
+++ usermode/usermode/communiction/communication.cpp
@@ -70,3 +70,29 @@ uint64_t c_communication::get_module_base( operation_callback operation, ULONGLO
 
 	return base_address;
 }
+
+uint64_t c_communication::alloc_memory( operation_callback operation, ULONGLONG target_pid, size_t size, uint32_t allocation_type, uint32_t protect, uintptr_t source_address ) {
+	packet_base_t packet{};
+
+	packet.opcode = e_opcode::ALLOC_VIRTUAL_MEMORY;
+	packet.side = e_side::SERVER;
+
+	auto& server_req = packet.server.alloc_memory;
+
+	server_req.target_pid = target_pid;
+	server_req.source_address = source_address;
+
+	server_req.allocation_type = allocation_type;
+	server_req.protect = protect;
+
+	server_req.size = size;
+	server_req.code = STATUS_INTERRUPTED;
+
+	operation( packet, 0xDEADBEEF );
+
+	auto client_req = packet.client.alloc_memory;
+
+	auto address = client_req.target_address;
+
+	return address;
+}
--- usermode/usermode/communiction/communication.hpp
+++ usermode/usermode/communiction/communication.hpp
@@ -7,4 +7,5 @@ public:
 	static bool test_operation( operation_callback );
 	static NTSTATUS copy_memory( operation_callback, ULONGLONG, uintptr_t, ULONGLONG, uintptr_t, SIZE_T );
 	static uint64_t get_module_base( operation_callback, ULONGLONG, std::wstring );
+	static uint64_t alloc_memory( operation_callback, ULONGLONG, size_t, uint32_t, uint32_t, uintptr_t );
 };
